1ca170062828685723a389deaf3cd369da1d12e7
chandra <chandra@localhost.localdomain>
2023-12-12 23:55:22 +0530

    created war project

---
+++ SBI/src/main/java/com/SBI/App.java
@@ -0,0 +1,13 @@
+package com.SBI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
